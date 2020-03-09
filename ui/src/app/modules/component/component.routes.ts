c42c6d058cb34260c548dac95aec4d079af765be
nbrouand <7816908+nbrouand@users.noreply.github.com>
2020-03-09 11:59:33 +0100

    bugfix(ui): NPE on saveComponent + Add more ui coherence between component/action saving

--- ui/src/app/modules/component/component.routes.ts
+++ ui/src/app/modules/component/component.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { CreateComponent } from './components/create-component/create-component.component';
 
 export const componentRoute: Routes = [
-        { path: '', component: CreateComponent },
-        { path: ':id', component: CreateComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'list' },
+    { path: ':id', component: CreateComponent }
 ];
