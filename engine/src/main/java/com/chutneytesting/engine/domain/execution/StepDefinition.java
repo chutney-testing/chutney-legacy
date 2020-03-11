24df8bdd91f5f23e0ae1031242aaba0f630edfb1
bessonm <besson.mael@gmail.com>
2020-03-11 16:59:37 +0100

    refactor: Improve engine stepDefinition toString()

--- engine/src/main/java/com/chutneytesting/engine/domain/execution/StepDefinition.java
+++ engine/src/main/java/com/chutneytesting/engine/domain/execution/StepDefinition.java
@@ -7,6 +7,7 @@ import com.chutneytesting.task.spi.injectable.Target;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -79,8 +80,30 @@ public class StepDefinition {
         return "StepDefinition{" +
             "name='" + name + '\'' +
             ", type='" + type + '\'' +
+            ", inputs=" + inputs +
+            ", steps=" + steps +
+            ", outputs=" + outputs +
+            ", target=" + target +
+            ", strategy=" + strategy +
             '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StepDefinition that = (StepDefinition) o;
+        return name.equals(that.name) &&
+            type.equals(that.type) &&
+            Objects.equals(inputs, that.inputs) &&
+            Objects.equals(steps, that.steps) &&
+            Objects.equals(outputs, that.outputs) &&
+            Objects.equals(target, that.target) &&
+            Objects.equals(strategy, that.strategy);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type, inputs, steps, outputs, target, strategy);
+    }
 }
