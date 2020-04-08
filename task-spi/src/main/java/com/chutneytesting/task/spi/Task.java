5024c31b5b5ad18db04415a7b8271adc6eb08348
bessonm <besson.mael@gmail.com>
2020-04-08 19:28:59 +0200

    doc: Document task identifier

--- task-spi/src/main/java/com/chutneytesting/task/spi/Task.java
+++ task-spi/src/main/java/com/chutneytesting/task/spi/Task.java
@@ -6,9 +6,16 @@ package com.chutneytesting.task.spi;
  * <p>
  * <font color="red"><b>Warning:</b> A task implementation must have one and only-one constructor</font>
  * </p>
+ *
+ * Tasks are identified by their class name converted from PascalCase to spinal-case.
+ * This is done by {@link com.chutneytesting.task.api.TaskTemplateMapper}
+ *
+ * ex. MySuperTask will become my-super-task
+ *
+ * This spinal-case identifier is used by external systems to specify which tasks to execute.
+ * For more information, @see com.chutneytesting.task.api package
  */
 // TODO add javadoc on constructor possible parameters
-// TODO add javadoc on identifier deduction
 public interface Task {
 
     /**
