6e0baf91ed839f0574fb936c82328dff4cde3f0e
BDA <58807088+boddissattva@users.noreply.github.com>
2020-04-23 10:24:22 +0200

    fix(ui): Component edition - Must recreate steps parameter values (#108)
    * fix(ui): Component edition - Must recreate steps parameter values
    onChanges callbacks.
    Fixes https://github.com/chutney-testing/chutney/issues/107
    
    * fix(ui): Component/Scenario edition - Share component-card component
    between component
    Fixes https://github.com/chutney-testing/chutney/issues/107

--- ui/src/app/modules/scenarios/components/edition/component-edition/component-edition.component.spec.ts
+++ ui/src/app/modules/scenarios/components/edition/component-edition/component-edition.component.spec.ts
@@ -1,139 +1,137 @@
-import { TestBed, async, fakeAsync, tick } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { TranslateModule } from '@ngx-translate/core';
-import { SharedModule } from '@shared/shared.module';
-
-import { MoleculesModule } from '../../../../../molecules/molecules.module';
-
-import { MomentModule } from 'angular2-moment';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { of } from 'rxjs';
-import { ComponentService } from '@core/services';
-import { ComponentEditionComponent } from './component-edition.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule, FormsModule, FormArray } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { MomentModule } from 'angular2-moment';
+import { TranslateModule } from '@ngx-translate/core';
 import { DragulaService, DragulaModule } from 'ng2-dragula';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { of } from 'rxjs';
+
+import { SharedModule } from '@shared/shared.module';
+import { MoleculesModule } from '../../../../../molecules/molecules.module';
+import { ComponentService } from '@core/services';
+import { ComponentTask, ScenarioComponent, KeyValue } from '@core/model';
+import { ComponentEditionComponent } from './component-edition.component';
 import { ScenarioCampaignsComponent } from '../../sub/scenario-campaigns/scenario-campaigns.component';
 import { ActivatedRouteStub } from 'src/app/testing/activated-route-stub';
-import { ActivatedRoute } from '@angular/router';
-import { ComponentTask, ScenarioComponent, KeyValue } from '@core/model';
 
 // ng2-dragula is hard to test.  @angular-skyhook should be a good replacement
 describe('ComponentEditionComponent', () => {
 
-  const componentService = jasmine.createSpyObj('ComponentService', ['findAllComponent', 'saveComponentTestCase', 'findComponentTestCase']);
-  const dragulaService = new DragulaService();
-  dragulaService.createGroup('FAKE', {});
-  const activatedRouteStub = new ActivatedRouteStub();
+    const componentService = jasmine.createSpyObj('ComponentService', ['findAllComponent', 'saveComponentTestCase', 'findComponentTestCase']);
+    const dragulaService = new DragulaService();
+    dragulaService.createGroup('FAKE', {});
+    const activatedRouteStub = new ActivatedRouteStub();
 
-  const task1 = new ComponentTask('name 1', null, [], [new KeyValue('1-param', '')], [new KeyValue('1-param', 'value1')],
-                                                                                                    [], null, 'id-1');
-  const task2 = new ComponentTask('name 2', null, [], [], [], [], null, 'id-2');
+    const task1 = new ComponentTask('name 1', null, [], [new KeyValue('1-param', '')], [new KeyValue('1-param', 'value1')],
+        [], null, 'id-1');
+    const task2 = new ComponentTask('name 2', null, [], [], [], [], null, 'id-2');
 
-  beforeEach(async(() => {
-    TestBed.resetTestingModule();
-    activatedRouteStub.setParamMap({ id: '42' });
-    activatedRouteStub.setSnapshotQueryParamMap({ duplicate: false });
+    beforeEach(async(() => {
+        TestBed.resetTestingModule();
+        activatedRouteStub.setParamMap({id: '42'});
+        activatedRouteStub.setSnapshotQueryParamMap({duplicate: false});
 
-    componentService.findAllComponent.and.returnValue(of([task1, task2]));
+        componentService.findAllComponent.and.returnValue(of([task1, task2]));
 
-    TestBed.configureTestingModule({
-      imports: [
-        HttpClientTestingModule,
-        RouterTestingModule,
-        TranslateModule.forRoot(),
-        MoleculesModule,
-        SharedModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MomentModule,
-        DragulaModule,
-        NgbModule,
-      ],
-      declarations: [
-        ScenarioCampaignsComponent,
-        ComponentEditionComponent,
-      ],
-      providers: [
-        { provide: ActivatedRoute, useValue: activatedRouteStub },
-        { provide: ComponentService, useValue: componentService },
-        { provide: DragulaService, useValue: dragulaService }
-      ]
-    }).compileComponents();
-  }));
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule,
+                RouterTestingModule,
+                TranslateModule.forRoot(),
+                MoleculesModule,
+                SharedModule,
+                FormsModule,
+                ReactiveFormsModule,
+                MomentModule,
+                DragulaModule,
+                NgbModule,
+            ],
+            declarations: [
+                ScenarioCampaignsComponent,
+                ComponentEditionComponent,
+            ],
+            providers: [
+                {provide: ActivatedRoute, useValue: activatedRouteStub},
+                {provide: ComponentService, useValue: componentService},
+                {provide: DragulaService, useValue: dragulaService}
+            ]
+        }).compileComponents();
+    }));
 
-  it('should create the component ComponentEditionComponent', () => {
-    componentService.findComponentTestCase.and.returnValue(of(new ScenarioComponent()));
+    it('should create the component ComponentEditionComponent', () => {
+        componentService.findComponentTestCase.and.returnValue(of(new ScenarioComponent()));
 
-    const fixture = TestBed.createComponent(ComponentEditionComponent);
-    fixture.detectChanges();
+        const fixture = TestBed.createComponent(ComponentEditionComponent);
+        fixture.detectChanges();
 
-    const c = fixture.componentInstance;
-    expect(c.canDeactivatePage()).toBe(true);
+        const c = fixture.componentInstance;
+        expect(c.canDeactivatePage()).toBe(true);
 
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it('Update tags should trigger scenario modified', () => {
-    const componentTestCaseMock = new ScenarioComponent('id', 'my title', 'my description', new Date(), [], [], [], ['tag1', 'tag2']);
-    componentService.findComponentTestCase.and.returnValue(of(componentTestCaseMock));
-    const fixture = TestBed.createComponent(ComponentEditionComponent);
-    fixture.detectChanges();
+    it('Update tags should trigger scenario modified', () => {
+        const componentTestCaseMock = new ScenarioComponent('id', 'my title', 'my description', new Date(), [], [], [], ['tag1', 'tag2']);
+        componentService.findComponentTestCase.and.returnValue(of(componentTestCaseMock));
+        const fixture = TestBed.createComponent(ComponentEditionComponent);
+        fixture.detectChanges();
 
-    const html: HTMLElement = fixture.nativeElement;
-    const scenarioSummary: HTMLTextAreaElement = html.querySelector('#tags');
-    expect(scenarioSummary.value).toBe('tag1,tag2');
+        const html: HTMLElement = fixture.nativeElement;
+        const scenarioSummary: HTMLTextAreaElement = html.querySelector('#tags');
+        expect(scenarioSummary.value).toBe('tag1,tag2');
 
-    const c = fixture.componentInstance;
-    c.componentForm.controls['tags'].setValue('tag1,tag2,anothertagg');
+        const c = fixture.componentInstance;
+        c.componentForm.controls['tags'].setValue('tag1,tag2,anothertagg');
 
-    expect(c.canDeactivatePage()).toBe(false);
-  });
+        expect(c.canDeactivatePage()).toBe(false);
+    });
 
-  it('Update sub steps should trigger scenario modified', () => {
+    it('Update sub steps should trigger scenario modified', () => {
 
-    const componentTestCaseMock = new ScenarioComponent('id', '', '', new Date(), [task1, task2], [], [], []);
-    componentService.findComponentTestCase.and.returnValue(of(componentTestCaseMock));
-    const fixture = TestBed.createComponent(ComponentEditionComponent);
-    fixture.detectChanges();
+        const componentTestCaseMock = new ScenarioComponent('id', '', '', new Date(), [task1, task2], [], [], []);
+        componentService.findComponentTestCase.and.returnValue(of(componentTestCaseMock));
+        const fixture = TestBed.createComponent(ComponentEditionComponent);
+        fixture.detectChanges();
 
-    const c = fixture.componentInstance;
-    c.componentTasksCreated.push(task1);
-    c.componentTasksCreated.push(task2);
+        const c = fixture.componentInstance;
+        c.componentTasksCreated.push(task1);
+        c.componentTasksCreated.push(task2);
 
-    expect(c.canDeactivatePage()).toBe(false);
-  });
+        expect(c.canDeactivatePage()).toBe(false);
+    });
 
-  it('Update scenario parameter should trigger scenario modified', () => {
-    const componentTestCaseMock = new ScenarioComponent('id', '', '', new Date(), [], [new KeyValue('key1', 'value1')], [], []);
-    componentService.findComponentTestCase.and.returnValue(of(componentTestCaseMock));
-    const fixture = TestBed.createComponent(ComponentEditionComponent);
-    fixture.detectChanges();
+    it('Update scenario parameter should trigger scenario modified', () => {
+        const componentTestCaseMock = new ScenarioComponent('id', '', '', new Date(), [], [new KeyValue('key1', 'value1')], [], []);
+        componentService.findComponentTestCase.and.returnValue(of(componentTestCaseMock));
+        const fixture = TestBed.createComponent(ComponentEditionComponent);
+        fixture.detectChanges();
 
-    const html: HTMLElement = fixture.nativeElement;
-    const addParameterButton: HTMLButtonElement = html.querySelector('#addParameterBtn');
-    addParameterButton.click();
-    fixture.detectChanges();
+        const html: HTMLElement = fixture.nativeElement;
+        const addParameterButton: HTMLButtonElement = html.querySelector('#addParameterBtn');
+        addParameterButton.click();
+        fixture.detectChanges();
 
-    const c = fixture.componentInstance;
-    const parametersArray = c.componentForm.controls['parameters'] as FormArray;
-    expect(parametersArray.length).toBe(2);
+        const c = fixture.componentInstance;
+        const parametersArray = c.componentForm.controls['parameters'] as FormArray;
+        expect(parametersArray.length).toBe(2);
 
-    expect(c.canDeactivatePage()).toBe(false);
-  });
+        expect(c.canDeactivatePage()).toBe(false);
+    });
 
-  it('Update component dataset  should trigger scenario modified', () => {
-    const componentTestCaseMock = new ScenarioComponent('id', '', '', new Date(), [task1, task2],
-                                                                [new KeyValue('param', 'value1')], [new KeyValue('dataset', 'value1')], []);
-    componentService.findComponentTestCase.and.returnValue(of(componentTestCaseMock));
-    const fixture = TestBed.createComponent(ComponentEditionComponent);
-    fixture.detectChanges();
+    it('Update component dataset should trigger scenario modified', () => {
+        const componentTestCaseMock = new ScenarioComponent('id', '', '', new Date(), [task1, task2],
+            [new KeyValue('param', 'value1')], [new KeyValue('dataset', 'value1')], []);
+        componentService.findComponentTestCase.and.returnValue(of(componentTestCaseMock));
+        const fixture = TestBed.createComponent(ComponentEditionComponent);
+        fixture.detectChanges();
 
-    const c = fixture.componentInstance;
-    const componentsValues = c.componentForm.controls.componentsValues as FormArray;
-    componentsValues.get('0').get('0').setValue('anotherValuefordataset');
+        const c = fixture.componentInstance;
+        c.componentTasksCreated[0].dataSet[0] = new KeyValue('dataset', 'value2');
 
-    expect(c.canDeactivatePage()).toBe(false);
-  });
+        expect(c.canDeactivatePage()).toBe(false);
+    });
 });
---
+++ ui/src/app/shared/components/component-card/component-card.component.ts
@@ -0,0 +1,70 @@
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+
+import { ComponentTask } from '@model';
+import { randomIntFromInterval } from '@shared/tools';
+import { Subscription } from 'rxjs';
+
+@Component({
+    selector: 'chutney-component-card',
+    templateUrl: './component-card.component.html',
+    styleUrls: ['./component-card.component.scss']
+})
+export class ComponentCardComponent implements OnChanges, OnDestroy {
+
+    @Input() component: ComponentTask;
+    @Output() deleteEvent = new EventEmitter();
+
+    cardForm: FormGroup;
+    uid: number = randomIntFromInterval(1, 500);
+    collapseComponentsParameters = true;
+
+    private parametersValueChangeSubscription: Array<Subscription> = [];
+
+    constructor(private formBuilder: FormBuilder
+    ) {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        this.initForm();
+    }
+
+    ngOnDestroy(): void {
+        this.cleanParametersSubscription();
+    }
+
+    switchCollapseComponentsParameter() {
+        this.collapseComponentsParameters = !this.collapseComponentsParameters;
+        if (!this.collapseComponentsParameters) {
+            this.initForm();
+        } else {
+            this.cleanParametersSubscription();
+        }
+    }
+
+    delete() {
+        this.deleteEvent.emit();
+    }
+
+    private initForm() {
+        this.cardForm = this.formBuilder.group({});
+        if (this.component && this.component.dataSet) {
+            this.component.dataSet.forEach((kv, index) => {
+                const ctrl: FormControl = this.formBuilder.control(kv.value);
+                this.cardForm.addControl(kv.key, ctrl);
+                this.parametersValueChangeSubscription.push(
+                    ctrl.valueChanges.pipe(
+                        debounceTime(250)
+                    ).subscribe(v => {
+                        this.component.dataSet[index].value = v;
+                    })
+                );
+            });
+        }
+    }
+
+    private cleanParametersSubscription() {
+        this.parametersValueChangeSubscription.forEach(s => s.unsubscribe());
+    }
+}
