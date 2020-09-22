ada161fa6a5af77281b519841b62d0ce74b765bc
bessonm <besson.mael@gmail.com>
2020-09-22 18:16:55 +0200

    fixup! feat(task-impl): create new json asserter (lessThan and GreaterThan)

--- task-impl/src/test/java/com/chutneytesting/task/assertion/JsonAssertTaskTest.java
+++ task-impl/src/test/java/com/chutneytesting/task/assertion/JsonAssertTaskTest.java
@@ -22,8 +22,13 @@ public class JsonAssertTaskTest {
         expected.put("$.something.thirddate", "$isEqualDate:2020-08-14T17:07:46.621+02:00");
 
         // Given
-        String fakeActualResult = "{" + "\"something\":{" + "\"onedate\":\"2020-08-14T16:56:56+02:00\","
-                + "\"seconddate\":\"2020-08-14T15:07:46.621Z\"," + "\"thirddate\":\"2020-08-14T15:07:46.621Z\"" + "}}";
+        String fakeActualResult = "{" +
+            "\"something\": {" +
+              "\"onedate\":\"2020-08-14T16:56:56+02:00\"," +
+              "\"seconddate\":\"2020-08-14T15:07:46.621Z\"," +
+              "\"thirddate\":\"2020-08-14T15:07:46.621Z\"" +
+            "}" +
+        "}";
 
         // When
         JsonAssertTask jsonAssertTask = new JsonAssertTask(new TestLogger(), fakeActualResult, expected);
@@ -48,10 +53,19 @@ public class JsonAssertTaskTest {
         expected.put("$.something.thenumber", "$isGreaterThan:45");
 
         // Given
-        String fakeActualResult = "{" + "\"something\":{" + "\"value\":3," + "\"alphabet\":\"abcdefg\","
-                + "\"valuenull\":null," + "\"matchregexp\":\"1983-10-26\","
-                + "\"onedate\":\"2000-01-01T10:11:12.123Z\"," + "\"seconddate\":\"2000-01-01T10:11:12.123Z\","
-                + "\"thirddate\":\"2000-01-01T10:11:12.123Z\"," + "\"anumber\":4 100," + "\"thenumber\":46" + "}" + "}";
+        String fakeActualResult = "{" +
+            "\"something\": {" +
+              "\"value\": 3," +
+              "\"alphabet\":\"abcdefg\"," +
+              "\"valuenull\":null," +
+              "\"matchregexp\":\"1983-10-26\"," +
+              "\"onedate\":\"2000-01-01T10:11:12.123Z\"," +
+              "\"seconddate\":\"2000-01-01T10:11:12.123Z\"," +
+              "\"thirddate\":\"2000-01-01T10:11:12.123Z\"," +
+              "\"anumber\":4 100," +
+              "\"thenumber\":46" +
+            "}" +
+        "}";
 
         // When
         JsonAssertTask jsonAssertTask = new JsonAssertTask(new TestLogger(), fakeActualResult, expected);
