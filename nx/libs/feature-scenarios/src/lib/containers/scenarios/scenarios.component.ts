ce16f093fb629d794e4fdff1581a26347c7aadde
iguissouma <issam.guissouma@laposte.net>
2020-10-11 20:24:56 +0200

    feature(ui): add delete confirm dialog

--- nx/libs/feature-scenarios/src/lib/containers/scenarios/scenarios.component.ts
+++ nx/libs/feature-scenarios/src/lib/containers/scenarios/scenarios.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Scenario, ScenariosGQL } from '@chutney/data-access';
 import { pluck } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { TdDialogService } from '@covalent/core/dialogs';
 
 @Component({
   selector: 'chutney-testing-scenarios',
@@ -11,11 +12,29 @@ import { Observable } from 'rxjs';
 export class ScenariosComponent implements OnInit {
   scenarios$: Observable<any[]>;
 
-  constructor(private scenariosGQL: ScenariosGQL) {}
+  constructor(private _dialogService: TdDialogService, private scenariosGQL: ScenariosGQL) {
+  }
 
   ngOnInit(): void {
     this.scenarios$ = this.scenariosGQL
       .watch()
       .valueChanges.pipe(pluck('data', 'scenarios'));
+  }
+
+  onEdit(id: number) {
+    console.log(`edit scenario with id${id}`);
+  }
+
+  onDelete(id: number) {
+    this._dialogService.openConfirm({
+      title: 'Confirm',
+      message: 'After deletion, the scenario cannot be restored',
+      cancelButton: 'Cancel',
+      acceptButton: 'Ok',
+    }).afterClosed().subscribe((accept: boolean) => {
+      if (accept) {
+        console.log(`delete scenario with id${id}`);
+      }
+    });
   }
 }
