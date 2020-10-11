821e7ec72af7c6c50c1eaf6d156bc1759654fcca
iguissouma <issam.guissouma@laposte.net>
2020-10-11 20:24:05 +0200

    feature(ui): add covalent dependency

--- nx/libs/ui-material/src/lib/ui-material.module.ts
+++ nx/libs/ui-material/src/lib/ui-material.module.ts
@@ -13,10 +13,12 @@ import { MatTableModule } from '@angular/material/table';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { CovalentDialogsModule } from '@covalent/core/dialogs';
 
 @NgModule({
   imports: [
     FlexLayoutModule,
+    /* Material Modules*/
     MatInputModule,
     MatCardModule,
     MatButtonModule,
@@ -30,9 +32,12 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatSelectModule,
     MatSnackBarModule,
     MatExpansionModule,
+    /** Covalent Modules */
+    CovalentDialogsModule,
   ],
   exports: [
     FlexLayoutModule,
+    /* Material modules*/
     MatInputModule,
     MatCardModule,
     MatButtonModule,
@@ -46,6 +51,8 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatSelectModule,
     MatSnackBarModule,
     MatExpansionModule,
+    /** Covalent Modules */
+    CovalentDialogsModule,
   ],
 })
 export class UiMaterialModule {}
