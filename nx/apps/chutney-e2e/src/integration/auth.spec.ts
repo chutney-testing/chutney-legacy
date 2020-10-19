07c4fb1186d8160cbd7d88fee72755b45ed0c51d
iguissouma <issam.guissouma@laposte.net>
2020-10-19 20:57:19 +0200

    feature(ui): add auth cypress spec #wip

---
+++ nx/apps/chutney-e2e/src/integration/auth.spec.ts
@@ -0,0 +1,8 @@
+describe('User Login', () => {
+  beforeEach(() => cy.visit('/auth/login'));
+
+  it('should display login page', () => {
+    cy.location("pathname").should("equal", "/auth/login");
+    cy.get('h4').contains('Login to your account');
+  });
+});
