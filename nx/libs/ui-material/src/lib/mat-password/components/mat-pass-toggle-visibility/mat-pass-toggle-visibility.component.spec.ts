2c7ffaf6c63c91ef70e28a9cb791d58463211de8
iguissouma <issam.guissouma@laposte.net>
2020-10-29 19:34:22 +0100

    feature(ui): add mat-toggle-visibility for password field

---
+++ nx/libs/ui-material/src/lib/mat-password/components/mat-pass-toggle-visibility/mat-pass-toggle-visibility.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MatPassToggleVisibilityComponent } from './mat-pass-toggle-visibility.component';
+
+describe('MatPassToggleVisibilityComponent', () => {
+  let component: MatPassToggleVisibilityComponent;
+  let fixture: ComponentFixture<MatPassToggleVisibilityComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MatPassToggleVisibilityComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MatPassToggleVisibilityComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
