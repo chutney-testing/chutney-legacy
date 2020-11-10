723239d7521d9ef147602a24e142d3a0d09fdcd2
smaouiomar <smaouiomar@gmail.com>
2020-11-10 20:20:08 +0100

    wip main layout design

---
+++ nx/libs/ui-layout/src/lib/components/sidemenu/sidemenu.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SidemenuComponent } from './sidemenu.component';
+
+describe('SidemenuComponent', () => {
+  let component: SidemenuComponent;
+  let fixture: ComponentFixture<SidemenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SidemenuComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SidemenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- nx/libs/ui-layout/src/lib/ui-layout.module.ts
+++ nx/libs/ui-layout/src/lib/ui-layout.module.ts
@@ -5,10 +5,14 @@ import { UiMaterialModule } from '@chutney/ui-material';
 import { RouterModule } from '@angular/router';
 import { AppBannerDirective } from './directives/appBanner/app-banner.directive';
 import { AuthLayoutComponent } from './containers/auth-layout/auth-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+import { UserPanelComponent } from './components/user-panel/user-panel.component';
 
 @NgModule({
   imports: [CommonModule, UiMaterialModule, RouterModule],
-  declarations: [MainLayoutComponent, AppBannerDirective, AuthLayoutComponent],
+  declarations: [MainLayoutComponent, AppBannerDirective, AuthLayoutComponent, HeaderComponent, SidebarComponent, SidemenuComponent, UserPanelComponent],
   exports: [MainLayoutComponent, AuthLayoutComponent],
 })
 export class UiLayoutModule {}
