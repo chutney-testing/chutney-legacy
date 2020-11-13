b3244d0e392e181cbdd50e356e5ec59d891a7e7b
iguissouma <issam.guissouma@laposte.net>
2020-11-13 20:49:15 +0100

    feature(ui): add scenario text run page #wip

--- nx/libs/feature-scenarios/src/lib/containers/scenario-text-run/scenario-text-run.component.ts
+++ nx/libs/feature-scenarios/src/lib/containers/scenario-text-run/scenario-text-run.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import {
+  RunScenarioGQL,
+  RunScenarioHistoryGQL,
+  Scenario,
+  ScenarioGQL,
+} from '@chutney/data-access';
+import { Observable } from 'rxjs';
+import { map, pluck } from 'rxjs/operators';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+import Hjson from 'hjson';
 
 @Component({
   selector: 'chutney-scenario-text-run',
@@ -6,7 +18,115 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scenario-text-run.component.scss'],
 })
 export class ScenarioTextRunComponent implements OnInit {
-  constructor() {}
+  private scenarioId: string;
+  private executionId: string;
+  private params$: Observable<Params>;
+  scenario$: Observable<Scenario>;
+  treeControl = new NestedTreeControl<any>((node) => node.subSteps);
+  dataSource = new MatTreeNestedDataSource<any>();
 
-  ngOnInit(): void {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private scenarioGQL: ScenarioGQL,
+    private runScenarioGQL: RunScenarioGQL,
+    private runScenarioHistoryGQL: RunScenarioHistoryGQL
+  ) {}
+
+  ngOnInit(): void {
+    this.scenarioId = this.route.snapshot.paramMap.get('id');
+    this.scenario$ = this.scenarioGQL
+      .watch({ scenarioId: this.scenarioId })
+      .valueChanges.pipe(pluck('data', 'scenario'));
+    this.scenario$
+      .pipe(
+        map((value) => {
+          return Hjson.parse(value.content);
+        })
+      )
+      .subscribe(
+        (scenario: any) =>
+          (this.dataSource.data = this.normalizeScenario(scenario))
+      );
+
+    this.params$ = this.route.params;
+    this.params$.subscribe((p) => {
+      this.scenarioId = p.id;
+      this.executionId = p.executionId;
+      new Observable<any>((obs) => {
+        let es;
+        try {
+          const url =
+            `/api/ui/scenario/executionasync/v1/${p.id}` +
+            `/execution/${p.executionId}`;
+          es = new EventSource(url);
+          es.addEventListener('partial', (evt: any) => {
+            obs.next(JSON.parse(evt.data));
+          });
+          es.addEventListener('last', (evt: any) => {
+            obs.next(JSON.parse(evt.data));
+            obs.complete();
+          });
+          es.addEventListener('error', (e) => {
+            console.log('An error occurred while attempting to connect.');
+            obs.error(e);
+            // throwError('An error occurred while attempting to connect.');
+          });
+        } catch (error) {
+          obs.error(error);
+          // throw Error('An error occurred while attempting to connect.');
+        }
+
+        return () => {
+          if (es) {
+            es.close();
+          }
+        };
+      }).subscribe(
+        (data) => {
+          console.log(JSON.stringify(data));
+        },
+        (msg) => {
+          this.runScenarioHistoryGQL
+            .fetch({
+              scenarioId: this.scenarioId,
+              executionId: this.executionId,
+            })
+            .pipe(pluck('data', 'runScenarioHistory'))
+            .subscribe((data) => {
+              const scenarioExecution = JSON.parse(data.report);
+              console.log(JSON.stringify(scenarioExecution));
+            });
+        },
+        () => {
+          console.log('completed...');
+        }
+      );
+    });
+  }
+
+  normalizeScenario(scenario: any): any[] {
+    return [
+      ...this.normalize(scenario.givens, 'Given'),
+      ...this.normalize([scenario.when], 'When'),
+      ...this.normalize(scenario.thens, 'Then'),
+    ];
+  }
+
+  normalize(steps: any[], keyword: string): any[] {
+    return steps.map((x) => Object.assign({}, x, { keyword: keyword }));
+  }
+
+  hasChild = (_: number, node: any) =>
+    !!node.subSteps && node.subSteps.length > 0;
+
+  runScenario(scenario: Scenario) {
+    this.runScenarioGQL
+      .mutate({ scenarioId: this.scenarioId, dataset: [] })
+      .subscribe((result) =>
+        this.router.navigate([`../${result.data.runScenario}`], {
+          relativeTo: this.route,
+        })
+      );
+  }
 }
