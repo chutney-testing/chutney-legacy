76bbb95e6cc64a44330c167df607ee2dcfebd649
iguissouma <issam.guissouma@laposte.net>
2020-11-14 22:00:51 +0100

    feature(ui): add simple translation switch

--- nx/libs/ui-layout/src/lib/components/translation/translation.component.ts
+++ nx/libs/ui-layout/src/lib/components/translation/translation.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  ChutneyAppLanguage,
+  TRANSLATION,
+  Translation,
+} from '@chutney/feature-i18n';
 
 @Component({
   selector: 'chutney-translation',
@@ -6,7 +12,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./translation.component.scss'],
 })
 export class TranslationComponent implements OnInit {
-  constructor() {}
+  // this will extract tuple of translation key and name from enum
+  translations = Object.entries(ChutneyAppLanguage);
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {}
+
+  switchLanguage(language: any) {
+    var url = this.router
+      .createUrlTree([], { relativeTo: this.route })
+      .toString();
+    Object.entries(ChutneyAppLanguage).forEach((e) => {
+      url = url.replace(`/${e[1]}/`, `/${language}/`);
+    });
+    this.router.navigateByUrl(url);
+  }
 }
