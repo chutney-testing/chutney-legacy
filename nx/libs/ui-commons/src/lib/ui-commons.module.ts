3a6b0e1556280ac1045d6aabbeccae706b586973
iguissouma <issam.guissouma@laposte.net>
2020-11-22 21:09:05 +0100

    feature(ui): add page header component

--- nx/libs/ui-commons/src/lib/ui-commons.module.ts
+++ nx/libs/ui-commons/src/lib/ui-commons.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ExecutionBadgeComponent } from './components/execution-badge/execution-badge.component';
 import { ExecutionIconComponent } from './components/execution-icon/execution-icon.component';
 import { MatIconModule } from '@angular/material/icon';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { UiMaterialModule } from '@chutney/ui-material';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, MatIconModule],
-  declarations: [ExecutionBadgeComponent, ExecutionIconComponent],
-  exports: [ExecutionBadgeComponent, ExecutionIconComponent],
+  imports: [CommonModule, MatIconModule, UiMaterialModule, RouterModule],
+  declarations: [ExecutionBadgeComponent, ExecutionIconComponent, PageHeaderComponent],
+  exports: [ExecutionBadgeComponent, ExecutionIconComponent, PageHeaderComponent],
 })
 export class UiCommonsModule {}
