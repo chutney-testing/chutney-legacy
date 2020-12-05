266c45318a058be5e6a4fcf01bd1d2877e46cf64
iguissouma <issam.guissouma@laposte.net>
2020-12-05 13:04:28 +0100

    feature(ui): refine run scenario

---
+++ nx/libs/ui-layout/src/lib/cache.ts
@@ -0,0 +1,13 @@
+import { makeVar } from '@apollo/client';
+
+export const layoutOprionsVar = makeVar({
+  navPos: 'side',
+  dir: 'ltr',
+  theme: 'light',
+  showHeader: true,
+  headerPos: 'fixed',
+  showUserPanel: true,
+  sidenavOpened: true,
+  sidenavCollapsed: false,
+  language: 'en-US',
+});
