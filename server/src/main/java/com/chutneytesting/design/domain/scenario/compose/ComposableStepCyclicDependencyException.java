fb8f15d3cb8ffe9b8491912eb21b7ad4d3c2d4db
bessonm <besson.mael@gmail.com>
2020-12-15 19:29:24 +0100

    refactor(server): Improve lisibility of cyclic dep check

--- server/src/main/java/com/chutneytesting/design/domain/scenario/compose/ComposableStepCyclicDependencyException.java
+++ server/src/main/java/com/chutneytesting/design/domain/scenario/compose/ComposableStepCyclicDependencyException.java
@@ -3,6 +3,6 @@ package com.chutneytesting.design.domain.scenario.compose;
 public class ComposableStepCyclicDependencyException extends RuntimeException {
 
     public ComposableStepCyclicDependencyException(String composableStepName) {
-        super("Cyclic dependency found on functional step [" + composableStepName + "]");
+        super("Cyclic dependency found on composable step [" + composableStepName + "]");
     }
 }
