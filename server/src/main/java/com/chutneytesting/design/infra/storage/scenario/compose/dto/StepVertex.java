8c11f63f05369dbcb40ed72bb8034a09e48a5f73
bessonm <besson.mael@gmail.com>
2020-12-15 15:26:27 +0100

    fixup! WIP - refactor(server): Bubble up dto builder and give it functionnality

--- server/src/main/java/com/chutneytesting/design/infra/storage/scenario/compose/dto/StepVertex.java
+++ server/src/main/java/com/chutneytesting/design/infra/storage/scenario/compose/dto/StepVertex.java
@@ -2,18 +2,31 @@ package com.chutneytesting.design.infra.storage.scenario.compose.dto;
 
 import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientComponentDB.GE_STEP_CLASS;
 import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientComponentDB.GE_STEP_CLASS_PROPERTY_PARAMETERS;
+import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientComponentDB.STEP_CLASS_PROPERTY_IMPLEMENTATION;
+import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientComponentDB.STEP_CLASS_PROPERTY_NAME;
 import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientComponentDB.STEP_CLASS_PROPERTY_PARAMETERS;
+import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientComponentDB.STEP_CLASS_PROPERTY_STRATEGY;
+import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientComponentDB.STEP_CLASS_PROPERTY_TAGS;
+import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientComponentDB.STEP_CLASS_PROPERTY_USAGE;
 import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientUtils.load;
 import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientUtils.reloadIfDirty;
+import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientUtils.setOrRemoveProperty;
+import static java.util.Optional.empty;
 import static java.util.Optional.ofNullable;
 
+import com.chutneytesting.design.domain.scenario.compose.ComposableStep;
 import com.chutneytesting.design.domain.scenario.compose.ComposableStepNotFoundException;
+import com.chutneytesting.design.domain.scenario.compose.StepUsage;
+import com.chutneytesting.design.domain.scenario.compose.Strategy;
 import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.ODirection;
 import com.orientechnologies.orient.core.record.OEdge;
+import com.orientechnologies.orient.core.record.OElement;
 import com.orientechnologies.orient.core.record.ORecord;
 import com.orientechnologies.orient.core.record.OVertex;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.StreamSupport;
@@ -53,16 +66,20 @@ public class StepVertex {
             });
     }
 
-    public static StepVertexBuilder builder() {
-        return new StepVertexBuilder();
-    }
-
     public void removeAllSubStepReferences() {
         vertex.getEdges(ODirection.OUT, GE_STEP_CLASS).forEach(ORecord::delete);
     }
 
     public OEdge addSubStep(StepVertex subStep) {
         return this.vertex.addEdge(subStep.vertex, GE_STEP_CLASS);
+    }
+
+    public OVertex save() {
+        return vertex.save();
+    }
+
+    public static StepVertexBuilder builder() {
+        return new StepVertexBuilder();
     }
 
     public static class StepVertexBuilder {
@@ -72,12 +89,33 @@ public class StepVertex {
 
         OVertex vertex;
 
+        private String name;
+        private Optional<StepUsage> usage = empty();
+        private Strategy strategy;
+        private List<String> tags;
+        private Optional<String> implementation = empty();
+        private Map<String, String> builtInParameters;
+        private Map<String, String> enclosedUsageParameters;
+
         private StepVertexBuilder() {};
 
         public StepVertex build() {
             if (this.vertex == null) {
                 this.vertex = (OVertex) load(id, dbSession).orElseThrow(() -> new ComposableStepNotFoundException(id));
             }
+
+            ofNullable(name).ifPresent(n -> vertex.setProperty(STEP_CLASS_PROPERTY_NAME, n, OType.STRING));
+            usage.ifPresent(u -> setOrRemoveProperty(vertex, STEP_CLASS_PROPERTY_USAGE, u, OType.STRING));
+            implementation.ifPresent(i -> setOrRemoveProperty(vertex, STEP_CLASS_PROPERTY_IMPLEMENTATION, i, OType.STRING));
+            ofNullable(tags).ifPresent(t -> setOrRemoveProperty(vertex, STEP_CLASS_PROPERTY_TAGS, t, OType.EMBEDDEDLIST));
+            ofNullable(builtInParameters).ifPresent( p -> vertex.setProperty(STEP_CLASS_PROPERTY_PARAMETERS, p, OType.EMBEDDEDMAP));
+
+            ofNullable(strategy).ifPresent( s -> {
+                OElement strategy = dbSession.newElement();
+                strategy.setProperty("name", s.type, OType.STRING);
+                strategy.setProperty("parameters", s.parameters, OType.EMBEDDEDMAP);
+                setOrRemoveProperty(vertex, STEP_CLASS_PROPERTY_STRATEGY, strategy, OType.EMBEDDED);
+            });
 
             return new StepVertex(vertex);
         }
@@ -94,6 +132,45 @@ public class StepVertex {
 
         public StepVertexBuilder usingSession(ODatabaseSession dbSession) {
             this.dbSession = dbSession;
+            return this;
+        }
+
+        public StepVertexBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public StepVertexBuilder withUsage(Optional<StepUsage> usage) {
+            this.usage = usage;
+            return this;
+        }
+
+        public StepVertexBuilder withTags(List<String> tags) {
+            this.tags = tags;
+            return this;
+        }
+
+        public StepVertexBuilder withImplementation(Optional<String> implementation) {
+            this.implementation = implementation;
+            return this;
+        }
+
+        public StepVertexBuilder withStrategy(Strategy strategy) {
+            this.strategy = strategy;
+            return this;
+        }
+
+        public StepVertexBuilder withBuiltInParameters(Map<String, String> builtInParameters) {
+            this.builtInParameters = builtInParameters;
+            return this;
+        }
+
+        public StepVertexBuilder withEnclosedUsageParameters(Map<String, String> enclosedUsageParameters) {
+            this.enclosedUsageParameters= enclosedUsageParameters;
+            return this;
+        }
+
+        public StepVertexBuilder withSteps(List<ComposableStep> steps) {
             return this;
         }
     }
