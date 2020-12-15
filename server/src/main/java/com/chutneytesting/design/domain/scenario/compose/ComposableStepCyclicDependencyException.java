6485109d46fd95af9a43fad78d0fca45648fe40d
bessonm <besson.mael@gmail.com>
2020-12-15 20:21:44 +0100

    refactor(server): Move cyclic dependency check in domain

--- server/src/main/java/com/chutneytesting/design/domain/scenario/compose/ComposableStepCyclicDependencyException.java
+++ server/src/main/java/com/chutneytesting/design/domain/scenario/compose/ComposableStepCyclicDependencyException.java
@@ -2,7 +2,7 @@ package com.chutneytesting.design.domain.scenario.compose;
 
 public class ComposableStepCyclicDependencyException extends RuntimeException {
 
-    public ComposableStepCyclicDependencyException(String composableStepName) {
-        super("Cyclic dependency found on composable step [" + composableStepName + "]");
+    public ComposableStepCyclicDependencyException(String id, String composableStepName) {
+        super("Cyclic dependency found on composable step #"+id+"[" + composableStepName + "]");
     }
 }
