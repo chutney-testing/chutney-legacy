5225c38f7409a4a418a8617c02ca1c82d8973e12
bessonm <besson.mael@gmail.com>
2020-12-15 12:08:47 +0100

    WIP - refactor(server): Create dto with logic before moving logic into domain

---
+++ server/src/main/java/com/chutneytesting/design/infra/storage/scenario/compose/dto/StepVertex.java
@@ -0,0 +1,81 @@
+package com.chutneytesting.design.infra.storage.scenario.compose.dto;
+
+import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientComponentDB.GE_STEP_CLASS;
+import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientComponentDB.GE_STEP_CLASS_PROPERTY_PARAMETERS;
+import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientComponentDB.STEP_CLASS_PROPERTY_PARAMETERS;
+import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientUtils.load;
+import static com.chutneytesting.design.infra.storage.scenario.compose.orient.OrientUtils.reloadIfDirty;
+import static java.util.Optional.ofNullable;
+
+import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.record.ODirection;
+import com.orientechnologies.orient.core.record.OVertex;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
+public class StepVertex {
+
+    public final OVertex vertex;
+
+    private StepVertex(OVertex vertex) {
+        this.vertex = vertex;
+    }
+
+    public Map<String, String> getDataset() {
+        reloadIfDirty(vertex);
+        Map<String, String> dataset = mergeComposableStepsChildrenDatasets();
+        Map<String, String> parameters = vertex.getProperty(STEP_CLASS_PROPERTY_PARAMETERS);
+        ofNullable(parameters).ifPresent(dataset::putAll);
+        return dataset;
+    }
+
+    private Map<String, String> mergeComposableStepsChildrenDatasets() {
+        return StreamSupport
+            .stream(vertex.getEdges(ODirection.OUT, GE_STEP_CLASS).spliterator(), false)
+            .map(childEdge -> {
+                StepVertex subStepVertex = StepVertex.builder().from(childEdge.getTo());
+                Map<String, String> dataSet = subStepVertex.getDataset();
+                Optional.<Map<String, String>>ofNullable(
+                    childEdge.getProperty(GE_STEP_CLASS_PROPERTY_PARAMETERS)
+                ).ifPresent(dataSet::putAll);
+                return dataSet;
+            })
+            .reduce(new HashMap<>(), (m1, m2) -> {
+                m1.putAll(m2);
+                return m1;
+            });
+    }
+
+    public static StepVertexBuilder builder() {
+        return new StepVertexBuilder();
+    }
+
+    public static class StepVertexBuilder {
+        String id;
+        ODatabaseSession dbSession;
+
+        private StepVertexBuilder() {};
+
+        public StepVertex build() {
+            return new StepVertex(
+                (OVertex) load(id, dbSession).orElseThrow(() -> new IllegalArgumentException("Component with id [" +id + "] does not exists"))
+            );
+        }
+
+        public StepVertex from(OVertex vertex) {
+            return new StepVertex(vertex);
+        }
+
+        public StepVertexBuilder withId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public StepVertexBuilder usingSession(ODatabaseSession dbSession) {
+            this.dbSession = dbSession;
+            return this;
+        }
+    }
+}
