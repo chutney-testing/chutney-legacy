1ea977b0f331ebaf3cb130f2cd7c184b3871874d
iguissouma <issam.guissouma@laposte.net>
2020-12-16 21:49:03 +0100

    feature(ui): extract sidemenu component

---
+++ nx/libs/ui-layout/src/lib/components/sidemenu/sidemenu.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'chutney-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: ['./sidemenu.component.scss'],
+})
+export class SidemenuComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
