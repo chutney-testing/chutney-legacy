90fc5cd04a7413b69c2871e94361d17ad7ab9ac7
iguissouma <issam.guissouma@laposte.net>
2020-12-27 19:17:47 +0100

    feature(ui): add campaign view page #wip

--- nx/libs/feature-campaigns/src/lib/containers/campaign-view/campaign-view.component.ts
+++ nx/libs/feature-campaigns/src/lib/containers/campaign-view/campaign-view.component.ts
@@ -1,12 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Campaign, CampaignGQL } from '@chutney/data-access';
+import { ActivatedRoute, Router } from '@angular/router';
+import { chutneyAnimations } from '@chutney/utils';
+import { map, pluck } from 'rxjs/operators';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 
 @Component({
   selector: 'chutney-campaign-view',
   templateUrl: './campaign-view.component.html',
   styleUrls: ['./campaign-view.component.scss'],
+  animations: [chutneyAnimations],
 })
 export class CampaignViewComponent implements OnInit {
-  constructor() {}
+  campaignId: string;
+  campaign$: Observable<Campaign>;
+  breadcrumbs: any = [
+    { title: 'Home', link: ['/'] },
+    { title: 'Campaigns', link: ['/'] },
+  ];
+  environments: string[] = ['GLOBAL', 'PERF'];
+  treeControl = new NestedTreeControl<any>((node) => node.subSteps);
+  dataSource = new MatTreeNestedDataSource<any>();
+  hasChild = (_: number, node: any) =>
+    !!node.subSteps && node.subSteps.length > 0;
 
-  ngOnInit(): void {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private campaignGQL: CampaignGQL
+  ) {}
+
+  ngOnInit(): void {
+    this.campaignId = this.route.snapshot.paramMap.get('id');
+    this.campaign$ = this.campaignGQL
+      .watch({ campaignId: this.campaignId })
+      .valueChanges.pipe(pluck('data', 'campaign'));
+    this.campaign$.subscribe(
+      (campaign: Campaign) => (this.dataSource.data = campaign.scenarios)
+    );
+  }
+
+  editCampaign(scenarioId: string) {
+    this.router.navigate([`../edit`], {
+      relativeTo: this.route,
+    });
+  }
+
+  runCampaign(campaignId: string, environment: string) {}
 }
