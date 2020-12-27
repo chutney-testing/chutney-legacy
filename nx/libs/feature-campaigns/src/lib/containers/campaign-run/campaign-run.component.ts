a8a463af8f45eb3240a2ffb49f7e88b30226e4d5
iguissouma <issam.guissouma@laposte.net>
2020-12-27 20:51:31 +0100

    feature(ui): add campaign run page #wip

--- nx/libs/feature-campaigns/src/lib/containers/campaign-run/campaign-run.component.ts
+++ nx/libs/feature-campaigns/src/lib/containers/campaign-run/campaign-run.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { chutneyAnimations } from '@chutney/utils';
 
 @Component({
   selector: 'chutney-campaign-run',
   templateUrl: './campaign-run.component.html',
-  styleUrls: ['./campaign-run.component.css']
+  styleUrls: ['./campaign-run.component.scss'],
+  animations: [chutneyAnimations]
 })
 export class CampaignRunComponent implements OnInit {
 
