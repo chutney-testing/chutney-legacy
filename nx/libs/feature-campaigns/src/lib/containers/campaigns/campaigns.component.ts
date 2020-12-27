20c94983885b2df72a789be8b9d1e508c22b8473
iguissouma <issam.guissouma@laposte.net>
2020-12-27 12:58:39 +0100

    feature(ui): add campaign view page #wip

--- nx/libs/feature-campaigns/src/lib/containers/campaigns/campaigns.component.ts
+++ nx/libs/feature-campaigns/src/lib/containers/campaigns/campaigns.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Campaign, CampaignsGQL } from '@chutney/data-access';
 import { pluck } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'chutney-campaigns',
@@ -9,28 +10,29 @@ import { pluck } from 'rxjs/operators';
   styleUrls: ['./campaigns.component.scss'],
 })
 export class CampaignsComponent implements OnInit {
-
-
   breadcrumbs: any = [
-    {title: 'Home', link: ['/']},
-    {title: 'Campaigns', link: ['/']},
+    { title: 'Home', link: ['/'] },
+    { title: 'Campaigns', link: ['/'] },
   ];
   campaigns$: Observable<any>;
 
   constructor(
+    private router: Router,
+    private route: ActivatedRoute,
     private campaignsGQL: CampaignsGQL
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
-    this.campaigns$ = this.campaignsGQL.watch().valueChanges.pipe(pluck('data', 'campaigns'));
+    this.campaigns$ = this.campaignsGQL
+      .watch()
+      .valueChanges.pipe(pluck('data', 'campaigns'));
   }
 
-  onEdit($event: any) {
+  onEdit($event: any) {}
 
-  }
+  onDelete($event: any) {}
 
-  onDelete($event: any) {
-
+  onView(id: string) {
+    this.router.navigate([id, 'view'], { relativeTo: this.route });
   }
 }
