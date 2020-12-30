a46c604f11ca973b9c0ee01d8fc96290da7f1693
iguissouma <issam.guissouma@laposte.net>
2020-12-30 14:11:13 +0100

    feature(ui): add edit page #wip

---
+++ nx/libs/feature-campaigns/src/lib/containers/campaign-edit/campaign-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'chutney-campaign-edit',
+  templateUrl: './campaign-edit.component.html',
+  styleUrls: ['./campaign-edit.component.scss'],
+})
+export class CampaignEditComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
