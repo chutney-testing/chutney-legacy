3af16921e5dc9ceb5babbb10fcaa22f3732a5ddf
boddissattva <58807088+boddissattva@users.noreply.github.com>
2021-02-03 11:37:26 +0100

    feat(ui): Add global variables read

--- nx/libs/feature-variables/src/lib/containers/variables/variables.component.ts
+++ nx/libs/feature-variables/src/lib/containers/variables/variables.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { pluck } from 'rxjs/operators';
+import { TdCodeEditorComponent } from '@covalent/code-editor';
+import { editor } from 'monaco-editor';
+
+import {
+  GlobalVariableGroupsNamesGQL,
+  GlobalVariableGroupContentGQL,
+  GlobalVariableGroupContent,
+} from '@chutney/data-access';
+import { layoutOprionsVar } from '@chutney/ui-layout';
+
+declare const monaco: any;
 
 @Component({
   selector: 'chutney-variables',
@@ -6,10 +19,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./variables.component.scss']
 })
 export class VariablesComponent implements OnInit {
+  breadcrumbs: any = [
+      { title: 'Home', link: ['/'] },
+      { title: 'Variables', link: ['/variables'] },
+    ];
 
-  constructor() { }
+  globalVariableGroupsNames$: Observable<String[]>;
+  private selectedGroupName: string;
+
+  @ViewChild(TdCodeEditorComponent, { static: false })
+  public monaco: TdCodeEditorComponent;
+  private _editor: any;
+  options: any = layoutOprionsVar();
+
+  constructor(
+    private globalVariableGroupsNamesGQL: GlobalVariableGroupsNamesGQL,
+    private globalVariableGroupContentGQL: GlobalVariableGroupContentGQL
+  ) { }
 
   ngOnInit(): void {
+    this.globalVariableGroupsNames$ = this.globalVariableGroupsNamesGQL
+          .watch()
+          .valueChanges.pipe(pluck('data', 'globalVariableGroupsNames'));
+  }
+
+  onTabChange(matTabChangeEvent) {
+    this.globalVariableGroupContentGQL
+      .fetch({ groupName: matTabChangeEvent.tab.textLabel })
+      .pipe(pluck('data', 'globalVariableGroupContent'))
+      .subscribe(
+        (content: GlobalVariableGroupContent) => {
+          this.monaco.value = content.message;
+        }
+      );
+  }
+
+  async editorInitialized(editorInstance: any): Promise<void> {
+    this._editor = editorInstance;
+  }
+
+  monacoEditorConfigChanged(theme: string) {
+    monaco.editor.setTheme(theme);
   }
 
 }
--- nx/libs/feature-variables/src/lib/feature-variables.module.ts
+++ nx/libs/feature-variables/src/lib/feature-variables.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { CovalentCodeEditorModule } from '@covalent/code-editor';
+
+import { UiCommonsModule } from '@chutney/ui-commons';
+import { UiMaterialModule } from '@chutney/ui-material';
+import { UtilsModule } from '@chutney/utils';
+
 import { VariablesComponent } from './containers/variables/variables.component';
 
 @NgModule({
@@ -8,7 +14,11 @@ import { VariablesComponent } from './containers/variables/variables.component';
     CommonModule,
     RouterModule.forChild([
       { path: '', component: VariablesComponent }
-    ])
+    ]),
+    UiCommonsModule,
+    UiMaterialModule,
+    UtilsModule,
+    CovalentCodeEditorModule,
   ],
   declarations: [VariablesComponent],
 })
