b0df957862128d5cc85376ae2e545e8fc875f832
Redouane benyoussef <redouane.benyoussef@gmail.com>
2021-03-03 11:25:12 +0100

    feat(task-impl): allow add headers to http soap task (#363)

--- task-impl/src/main/java/com/chutneytesting/task/http/HttpSoapTask.java
+++ task-impl/src/main/java/com/chutneytesting/task/http/HttpSoapTask.java
@@ -2,16 +2,18 @@ package com.chutneytesting.task.http;
 
 import static java.util.Optional.ofNullable;
 
+import com.chutneytesting.task.function.XmlFunction;
+import com.chutneytesting.task.http.domain.HttpClient;
+import com.chutneytesting.task.http.domain.HttpClientFactory;
+import com.chutneytesting.task.http.domain.HttpTask;
 import com.chutneytesting.task.spi.Task;
 import com.chutneytesting.task.spi.TaskExecutionResult;
 import com.chutneytesting.task.spi.injectable.Input;
 import com.chutneytesting.task.spi.injectable.Logger;
 import com.chutneytesting.task.spi.injectable.Target;
 import com.chutneytesting.task.spi.time.Duration;
-import com.chutneytesting.task.function.XmlFunction;
-import com.chutneytesting.task.http.domain.HttpClient;
-import com.chutneytesting.task.http.domain.HttpClientFactory;
-import com.chutneytesting.task.http.domain.HttpTask;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Supplier;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpHeaders;
@@ -28,13 +30,15 @@ public class HttpSoapTask implements Task {
     private final String password;
     private final Object body;
     private final int timeout;
+    private final Map<String, String> headers;
 
     public HttpSoapTask(Target target, Logger logger,
                         @Input("uri") String uri,
                         @Input("body") String body,
                         @Input("username") String username,
                         @Input("password") String password,
-                        @Input("timeout") String timeout) {
+                        @Input("timeout") String timeout,
+                        @Input("headers") Map<String, String> headers) {
         this.target = target;
         this.logger = logger;
         this.uri = uri;
@@ -46,12 +50,14 @@ public class HttpSoapTask implements Task {
             .map(Duration::parse)
             .map(Duration::toMilliseconds)
             .orElse(DEFAULT_TIMEOUT).intValue();
+        this.headers = headers != null ? headers : new HashMap<>();
     }
 
     @Override
     public TaskExecutionResult execute() {
         HttpClient httpClient = new HttpClientFactory().create(target, String.class, timeout);
         HttpHeaders httpHeaders = new HttpHeaders();
+        headers.entrySet().forEach(e -> httpHeaders.add(e.getKey(), e.getValue()));
         Object finalBody = XmlFunction.getSoapBody(this.username, this.password, body.toString());
         Supplier<ResponseEntity<String>> caller = () -> httpClient.post(this.uri, ofNullable(finalBody).orElse("{}"), httpHeaders);
         return HttpTask.httpCall(logger, caller);
