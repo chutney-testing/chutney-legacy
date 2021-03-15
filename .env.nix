b5e68f26e371bbbaf12b1b360bb37f743a87392d
Mael Besson <besson.mael@gmail.com>
2021-03-15 10:01:11 +0100

    chore: Setup jdk11 config with nix (#370)

--- .env.nix
+++ .env.nix
@@ -1,16 +1,21 @@
-let
-  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
-in
 { nixpkgs ? import <nixpkgs> {} }:
-with nixpkgs; mkShell {
+with nixpkgs;
+let
+  jdk = openjdk11;
+  mvn = maven.override { jdk  = jdk; };
+in
+mkShell {
 
   buildInputs = [
-    unstable.nodejs-12_x
+    nodejs-12_x
     chromium
     geckodriver
+    jdk
+    mvn
   ];
 
   NPM_CONFIG_PROXY = builtins.getEnv "http_proxy";
   CHROME_BIN = "${chromium}/bin/chromium";
-
+  JAVA_HOME="${jdk}/lib/openjdk";
+  M2_HOME="${mvn}/maven";
 }
