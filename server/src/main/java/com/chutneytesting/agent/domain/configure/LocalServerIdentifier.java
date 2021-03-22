a28ff7d6d0dfed1c1ce1fa5ec1336d6b47d48c57
boddissattva <58807088+boddissattva@users.noreply.github.com>
2021-03-22 12:58:24 +0100

    chore(): Cache local hosts name and ips

--- server/src/main/java/com/chutneytesting/agent/domain/configure/LocalServerIdentifier.java
+++ server/src/main/java/com/chutneytesting/agent/domain/configure/LocalServerIdentifier.java
@@ -1,5 +1,6 @@
 package com.chutneytesting.agent.domain.configure;
 
+import static java.util.Collections.emptyList;
 import static java.util.Collections.list;
 
 import com.chutneytesting.engine.domain.delegation.NamedHostAndPort;
@@ -9,7 +10,9 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import com.chutneytesting.tools.ThrowingFunction;
 import com.chutneytesting.tools.ThrowingPredicate;
@@ -18,11 +21,24 @@ public class LocalServerIdentifier {
     private final int port;
     private final String defaultLocalName;
     private final String defaultLocalHostName;
+    private List<String> localHostsAndIps;
 
     public LocalServerIdentifier(int port, String defaultLocalName, String defaultLocalHostName) {
         this.port = port;
         this.defaultLocalName = defaultLocalName;
         this.defaultLocalHostName = defaultLocalHostName;
+
+        try {
+            this.localHostsAndIps = list(NetworkInterface.getNetworkInterfaces())
+                .stream()
+                .filter(ThrowingPredicate.toUnchecked(NetworkInterface::isUp))
+                .map(ThrowingFunction.toUnchecked(NetworkInterface::getInetAddresses))
+                .flatMap(addresses -> list(addresses).stream())
+                .flatMap(address -> Stream.of(address.getCanonicalHostName(), address.getHostAddress()))
+                .collect(Collectors.toList());
+        } catch (SocketException e) {
+            this.localHostsAndIps = emptyList();
+        }
     }
 
     public String getLocalName(NetworkConfiguration networkConfiguration) {
@@ -51,16 +67,11 @@ public class LocalServerIdentifier {
 
     private boolean isLocalHost(NamedHostAndPort namedHostAndPort) {
         try {
-            return list(NetworkInterface.getNetworkInterfaces())
-                .stream()
-                .filter(ThrowingPredicate.toUnchecked(NetworkInterface::isUp))
-                .map(ThrowingFunction.toUnchecked(NetworkInterface::getInetAddresses))
-                .flatMap(addresses -> list(addresses).stream())
-                .flatMap(address -> Stream.of(address.getCanonicalHostName(), address.getHostAddress()))
+            return localHostsAndIps.stream()
                 .anyMatch(host -> host.equalsIgnoreCase(namedHostAndPort.host()))
                 ||
                 InetAddress.getLocalHost().getCanonicalHostName().equalsIgnoreCase(namedHostAndPort.host());
-        } catch (SocketException | IllegalStateException | UnknownHostException e) {
+        } catch (IllegalStateException | UnknownHostException e) {
             return false;
         }
     }
