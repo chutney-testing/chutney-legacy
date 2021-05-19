63d778e924f16d054fa699dc696d815657724ac2
nbrouand <nbrouand@gmail.com>
2021-05-19 09:49:58 +0200

    bugfix(server): Fix home page serialization (#430)

--- server/src/main/java/com/chutneytesting/admin/domain/HomePage.java
+++ server/src/main/java/com/chutneytesting/admin/domain/HomePage.java
@@ -1,8 +1,11 @@
 package com.chutneytesting.admin.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class HomePage {
     public final String content;
 
+    @JsonCreator
     public HomePage(String content) {
         this.content = content;
     }
