e0d4fffcc0872e4a614b8ea3590e19396455fd3a
boddissattva <58807088+boddissattva@users.noreply.github.com>
2021-06-11 17:29:12 +0200

    fix(task-impl): Keep numeric and date JDBC SQL Types in task result

--- task-impl/src/test/resources/db/sql/create_db.sql
+++ task-impl/src/test/resources/db/sql/create_db.sql
@@ -1,6 +1,26 @@
 DROP TABLE users;
+
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY,
   name VARCHAR(30),
   email  VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS allsqltypes (
+  col_boolean BIT,
+  col_tinyint TINYINT,
+  col_smallint SMALLINT,
+  col_mediumint MEDIUMINT,
+  col_integer INTEGER,
+  col_bigint BIGINT,
+  col_float REAL,
+  col_double DOUBLE,
+  col_decimal DECIMAL(20,4),
+  col_date DATE,
+  col_time TIME,
+  col_timestamp TIMESTAMP,
+  col_interval_year INTERVAL YEAR,
+  col_interval_second INTERVAL SECOND,
+  col_char CHAR,
+  col_varchar VARCHAR
 );
