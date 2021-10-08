eaddb5b2608e5cb6e27b1e10aa05290dbb29335f
nbrouand <7816908+nbrouand@users.noreply.github.com>
2021-10-08 14:57:37 +0200

    feat(task-impl): make json compare soft assert

--- task-impl/src/main/java/com/chutneytesting/task/assertion/JsonCompareTask.java
+++ task-impl/src/main/java/com/chutneytesting/task/assertion/JsonCompareTask.java
@@ -16,6 +16,7 @@ import com.jayway.jsonpath.ReadContext;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * TODO :
@@ -81,14 +82,14 @@ public class JsonCompareTask implements Task {
     public TaskExecutionResult execute() {
         ReadContext doc1 = JsonPath.parse(JsonUtils.jsonStringify(document1));
         ReadContext doc2 = JsonPath.parse(JsonUtils.jsonStringify(document2));
-        boolean matchesOk = comparingPaths.entrySet().stream()
-            .allMatch(cp -> {
-                    boolean result = false;
+        AtomicBoolean result = new AtomicBoolean(true);
+        comparingPaths.entrySet().stream()
+            .forEach(cp -> {
                     try {
                         Object read1 = doc1.read(cp.getKey());
                         Object read2 = doc2.read(cp.getValue());
-                        result = read1.equals(read2);
-                        if (!result) {
+                        if (!read1.equals(read2)) {
+                            result.set(false);
                             logger.error("Value [" + read1 + "] at path [" + cp.getKey() + "] is not equal to value [" + read2 + "] at path [" + cp.getValue() + "]");
                         } else {
                             logger.info(read1.toString());
@@ -97,11 +98,10 @@ public class JsonCompareTask implements Task {
                     } catch (JsonPathException ex) {
                         logger.error(ex.getMessage());
                     }
-                    return result;
                 }
             );
 
-        if (!matchesOk) {
+        if (!result.get()) {
             return TaskExecutionResult.ko();
         }
         return TaskExecutionResult.ok();
--- task-impl/src/test/java/com/chutneytesting/task/assertion/JsonCompareTaskTest.java
+++ task-impl/src/test/java/com/chutneytesting/task/assertion/JsonCompareTaskTest.java
@@ -3,7 +3,10 @@ package com.chutneytesting.task.assertion;
 import static com.chutneytesting.task.spi.TaskExecutionResult.Status.Failure;
 import static com.chutneytesting.task.spi.TaskExecutionResult.Status.Success;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.chutneytesting.task.spi.TaskExecutionResult;
 import com.chutneytesting.task.spi.injectable.Logger;
@@ -14,7 +17,7 @@ import org.junit.jupiter.api.Test;
 public class JsonCompareTaskTest {
 
     @Test
-    public void should_execute_3_successful_assertions_on_comparing_actual_result_to_expected() {
+    public void should_execute_2_successful_assertions_on_comparing_actual_result_to_expected() {
         Map<String, String> pathInputs = new HashMap<>();
         pathInputs.put("$.something.value", "$.something_else.value");
         pathInputs.put("$.a_thing", "$.a_thing");
@@ -36,15 +39,19 @@ public class JsonCompareTaskTest {
 
     @Test
     public void should_failed_on_bad_assertions() {
-        Map<String, String> pathInputs = new HashMap<>();
-        pathInputs.put("$.something.value", "$.something.value");
-
         // Given
-        String doc1 = "{\"something\":{\"value\":3},\"something_else\":{\"value\":5},\"a_thing\":{\"type\":\"my_type\"}}";
-        String doc2 = "{\"something\":{\"value\":4},\"something_else\":{\"value\":5},\"a_thing\":{\"type\":\"my_type\"}}";
+        Map<String, String> pathInputs = new HashMap<>();
+        pathInputs.put("$.something.value", "$.something.value");//ko
+        pathInputs.put("$.something_else.value", "$.something_else.value");//ok
+        pathInputs.put("$.a_thing.type", "$.a_thing.type");//ko
 
+        String doc1 = "{\"something\":{\"value\":3},\"something_else\":{\"value\":5},\"a_thing\":{\"type\":\"type\"}}";
+        String doc2 = "{\"something\":{\"value\":4},\"something_else\":{\"value\":5},\"a_thing\":{\"type\":\"another_type\"}}";
+
+        Logger mockLogger = mock(Logger.class);
         // When
-        JsonCompareTask jsonAssertTask = new JsonCompareTask(mock(Logger.class),
+        JsonCompareTask jsonAssertTask = new JsonCompareTask(
+            mockLogger,
             doc1,
             doc2,
             pathInputs);
@@ -52,5 +59,6 @@ public class JsonCompareTaskTest {
 
         // Then
         assertThat(result.status).isEqualTo(Failure);
+        verify(mockLogger, times(2)).error(anyString());
     }
 }
