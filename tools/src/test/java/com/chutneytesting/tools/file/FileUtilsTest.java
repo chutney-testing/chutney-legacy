2433f9936b465bcb943b4464e6eba6f9619a3237
boddissattva <58807088+boddissattva@users.noreply.github.com>
2022-05-31 17:38:40 +0200

    fix(tools): init folder parallel execution support.   fixes https://github.com/chutney-testing/chutney/issues/746

---
+++ tools/src/test/java/com/chutneytesting/tools/file/FileUtilsTest.java
@@ -0,0 +1,44 @@
+package com.chutneytesting.tools.file;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CyclicBarrier;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.io.TempDir;
+
+class FileUtilsTest {
+
+    @RepeatedTest(5)
+    void init_folder_should_support_parallel_execution(@TempDir Path tmpDir) {
+        int nbSync = 2;
+        Path aDirectory = tmpDir.resolve("aDirectory");
+        CyclicBarrier sync = new CyclicBarrier(nbSync);
+        List<Exception> exceptions = new ArrayList<>();
+        Runnable run = () -> {
+            try {
+                sync.await();
+                FileUtils.initFolder(aDirectory);
+            } catch (Exception e) {
+                exceptions.add(e);
+            }
+        };
+
+        Collection<Thread> threads = IntStream.range(0, nbSync).mapToObj(i -> new Thread(run)).collect(toList());
+        threads.forEach(Thread::start);
+        threads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                exceptions.add(e);
+            }
+        });
+
+        assertThat(exceptions).isEmpty();
+    }
+}
