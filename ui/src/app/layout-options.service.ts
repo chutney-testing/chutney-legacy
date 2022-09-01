7d5196824bf74721a515f1321e196bb52ace6628
nbrouand <nbrouand@gmail.com>
2022-09-01 16:18:11 +0200

    Chore/login2 (#800)
    * First try on login page
    
    * login page + responsive
    
    * Move flatly into themes
    
    * 2022-08-10 refacto ui together
    
    * ui : Remove home page, add logo, align left and right panel
    
    * chore(ui+server+packaging): Add version and application name
    
    * feat(layout): add new layout (#806)
    
    Co-authored-by: Redouane BENYOUSSEF <redouane.benyoussef@gmail.com>
    Co-authored-by: Goubbaa Karim <karim.goubaa@gmail.com>

---
+++ ui/src/app/layout-options.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LayoutOptions {
+  sidebarHover = false;
+  toggleSidebar = false;
+  toggleSidebarMobile = false;
+}
