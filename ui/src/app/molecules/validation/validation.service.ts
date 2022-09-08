845d28144f8f84a7d106f38d960bc408fc345faa
bessonm <besson.mael@gmail.com>
2022-09-08 10:49:24 +0200

    fixup! fix(ui): fix linkifier regex validation

--- ui/src/app/molecules/validation/validation.service.ts
+++ ui/src/app/molecules/validation/validation.service.ts
@@ -22,12 +22,11 @@ export class ValidationService {
     }
 
     isValidPattern(text: string) {
-        let isValid = true;
         try {
             new RegExp(text);
         } catch {
-            isValid = false;
+            return false;
         }
-        return isValid;
+        return true;
     }
 }
