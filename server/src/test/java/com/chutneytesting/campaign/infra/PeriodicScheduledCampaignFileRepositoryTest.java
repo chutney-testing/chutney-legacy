142a68fb2741e5f84d480773dd8e59fac9d46a7f
boddissattva <58807088+boddissattva@users.noreply.github.com>
2023-03-31 15:52:55 +0200

    chore(): Clean System.setProperty in tests

--- server/src/test/java/com/chutneytesting/campaign/infra/PeriodicScheduledCampaignFileRepositoryTest.java
+++ server/src/test/java/com/chutneytesting/campaign/infra/PeriodicScheduledCampaignFileRepositoryTest.java
@@ -29,7 +29,6 @@ public class PeriodicScheduledCampaignFileRepositoryTest {
     @BeforeAll
     public static void setUp(@TempDir Path temporaryFolder) throws IOException {
         String tmpConfDir = temporaryFolder.toFile().getAbsolutePath();
-        System.setProperty("configuration-folder", tmpConfDir);
 
         sut = new SchedulingCampaignFileRepository(tmpConfDir);
         SCHEDULING_CAMPAIGN_FILE = Paths.get(tmpConfDir + "/scheduling/schedulingCampaigns.json");
@@ -43,26 +42,28 @@ public class PeriodicScheduledCampaignFileRepositoryTest {
         PeriodicScheduledCampaign sc2 = new PeriodicScheduledCampaign(null, 22L, "campaign title 2", LocalDateTime.of(2021, 3, 5, 8, 11));
         PeriodicScheduledCampaign sc3 = new PeriodicScheduledCampaign(null, 33L, "campaign title 3", LocalDateTime.of(2022, 4, 6, 9, 12));
         String expectedAdded =
-            "{\n" +
-                "  \"1\" : {\n" +
-                "    \"id\" : \"1\",\n" +
-                "    \"campaignId\" : 11,\n" +
-                "    \"campaignTitle\" : \"campaign title 1\",\n" +
-                "    \"schedulingDate\" : [ 2020, 2, 4, 7, 10 ]\n" +
-                "  },\n" +
-                "  \"2\" : {\n" +
-                "    \"id\" : \"2\",\n" +
-                "    \"campaignId\" : 22,\n" +
-                "    \"campaignTitle\" : \"campaign title 2\",\n" +
-                "    \"schedulingDate\" : [ 2021, 3, 5, 8, 11 ]\n" +
-                "  },\n" +
-                "  \"3\" : {\n" +
-                "    \"id\" : \"3\",\n" +
-                "    \"campaignId\" : 33,\n" +
-                "    \"campaignTitle\" : \"campaign title 3\",\n" +
-                "    \"schedulingDate\" : [ 2022, 4, 6, 9, 12 ]\n" +
-                "  }\n" +
-                "}";
+            """
+                {
+                  "1" : {
+                    "id" : "1",
+                    "campaignId" : 11,
+                    "campaignTitle" : "campaign title 1",
+                    "schedulingDate" : [ 2020, 2, 4, 7, 10 ]
+                  },
+                  "2" : {
+                    "id" : "2",
+                    "campaignId" : 22,
+                    "campaignTitle" : "campaign title 2",
+                    "schedulingDate" : [ 2021, 3, 5, 8, 11 ]
+                  },
+                  "3" : {
+                    "id" : "3",
+                    "campaignId" : 33,
+                    "campaignTitle" : "campaign title 3",
+                    "schedulingDate" : [ 2022, 4, 6, 9, 12 ]
+                  }
+                }
+                """;
 
         // When
         sut.add(sc1);
@@ -76,20 +77,22 @@ public class PeriodicScheduledCampaignFileRepositoryTest {
         //// REMOVE
         // Given
         String expectedAfterRemove =
-            "{\n" +
-                "  \"1\" : {\n" +
-                "    \"id\" : \"1\",\n" +
-                "    \"campaignId\" : 11,\n" +
-                "    \"campaignTitle\" : \"campaign title 1\",\n" +
-                "    \"schedulingDate\" : [ 2020, 2, 4, 7, 10 ]\n" +
-                "  },\n" +
-                "  \"3\" : {\n" +
-                "    \"id\" : \"3\",\n" +
-                "    \"campaignId\" : 33,\n" +
-                "    \"campaignTitle\" : \"campaign title 3\",\n" +
-                "    \"schedulingDate\" : [ 2022, 4, 6, 9, 12 ]\n" +
-                "  }\n" +
-                "}";
+            """
+                {
+                  "1" : {
+                    "id" : "1",
+                    "campaignId" : 11,
+                    "campaignTitle" : "campaign title 1",
+                    "schedulingDate" : [ 2020, 2, 4, 7, 10 ]
+                  },
+                  "3" : {
+                    "id" : "3",
+                    "campaignId" : 33,
+                    "campaignTitle" : "campaign title 3",
+                    "schedulingDate" : [ 2022, 4, 6, 9, 12 ]
+                  }
+                }
+                """;
 
         // When
         sut.removeById(2L);
