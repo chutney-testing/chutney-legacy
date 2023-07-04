6610ac1976b5166333f54a22c18e1066ced78097
nbrouand <7816908+nbrouand@users.noreply.github.com>
2023-07-04 16:53:34 +0200

    chore(engine): remove use of complex object in engine

--- engine/src/main/java/com/chutneytesting/action/api/ActionTemplateMapper.java
+++ engine/src/main/java/com/chutneytesting/action/api/ActionTemplateMapper.java
@@ -1,20 +1,14 @@
 package com.chutneytesting.action.api;
 
-import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.partitioningBy;
-import static java.util.stream.Collectors.toList;
 
 import com.chutneytesting.action.api.ActionDto.InputsDto;
 import com.chutneytesting.action.domain.ActionTemplate;
 import com.chutneytesting.action.domain.parameter.Parameter;
 import com.chutneytesting.action.spi.injectable.Input;
 import com.chutneytesting.action.spi.injectable.Target;
-import java.lang.reflect.Constructor;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public class ActionTemplateMapper {
 
@@ -37,34 +31,13 @@ public class ActionTemplateMapper {
             .filter(parameter -> parameter.annotations().optional(Input.class).isPresent())
             .collect(partitioningBy(ActionTemplateMapper::isSimpleType));
 
-        return Stream.concat(
-            parametersMap.get(true).stream()
-                .map(ActionTemplateMapper::simpleParameterToInputsDto),
-            parametersMap.get(false).stream()
-                .map(ActionTemplateMapper::complexParameterToInputsDto)
-                .flatMap(Collection::stream))
-            .collect(toList());
+        return parametersMap.get(true).stream().map(ActionTemplateMapper::simpleParameterToInputsDto).toList();
     }
 
     private static InputsDto simpleParameterToInputsDto(Parameter parameter) {
         return new InputsDto(parameter.annotations().get(Input.class).value(), parameter.rawType());
     }
 
-    private static List<InputsDto> complexParameterToInputsDto(Parameter parameter) {
-        Constructor<?>[] constructors = parameter.rawType().getConstructors();
-        if (constructors.length == 1) {
-            Constructor constructor = constructors[0];
-            List<InputsDto> result = Arrays.stream(constructor.getParameters())
-                .map(Parameter::fromJavaParameter)
-                .filter(p -> p.annotations().optional(Input.class).isPresent())
-                .map(p -> new InputsDto(p.annotations().get(Input.class).value(), p.rawType()))
-                .collect(toList());
-            if (!result.isEmpty()) {
-                return result;
-            }
-        }
-        return singletonList(simpleParameterToInputsDto(parameter));
-    }
 
     private static boolean isSimpleType(Parameter parameter) {
         Class<?> rawType = parameter.rawType();
--- engine/src/test/java/com/chutneytesting/action/api/ActionTemplateMapperTest.java
+++ engine/src/test/java/com/chutneytesting/action/api/ActionTemplateMapperTest.java
@@ -1,13 +1,12 @@
 package com.chutneytesting.action.api;
 
-import static com.chutneytesting.action.TestActionTemplateHelper.mockParameter;
 import static com.chutneytesting.action.TestActionTemplateHelper.mockActionTemplate;
+import static com.chutneytesting.action.TestActionTemplateHelper.mockParameter;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.chutneytesting.action.api.ComplexParameterTestClass.SubComplexObject;
 import com.chutneytesting.action.api.ActionDto.InputsDto;
 import com.chutneytesting.action.domain.ActionTemplate;
 import com.chutneytesting.action.domain.parameter.Parameter;
@@ -58,28 +57,4 @@ public class ActionTemplateMapperTest {
         assertThat(actionDto.getInputs()).containsExactlyInAnyOrder(new InputsDto(INPUT_NAME_2, INPUT_TYPE_2), new InputsDto(INPUT_NAME_3, INPUT_TYPE_3));
     }
 
-    @Test
-    public void should_map_action_template_with_complex_inputs_parameters() {
-        // Given
-        final String TASK_ID = "action-id";
-        final String INPUT_NAME_1 = "complexParam";
-        final Class<?> INPUT_TYPE_1 = ComplexParameterTestClass.class;
-        final String INPUT_NAME_2 = "complexParam2";
-        final Class<?> INPUT_TYPE_2 = Integer.class;
-        ActionTemplate actionTemplate = mockActionTemplate(TASK_ID, new HashSet<>(Arrays.asList(mockParameter(INPUT_TYPE_1, INPUT_NAME_1), mockParameter(INPUT_TYPE_2, INPUT_NAME_2))));
-
-        // When
-        ActionDto actionDto = ActionTemplateMapper.toDto(actionTemplate);
-
-        // Then
-        assertThat(actionDto.getIdentifier()).isEqualTo(TASK_ID);
-        assertThat(actionDto.target()).isFalse();
-        assertThat(actionDto.getInputs())
-            .containsExactlyInAnyOrder(
-                new InputsDto("first", String.class),
-                new InputsDto("second", Integer.class),
-                new InputsDto("complexParam2", Integer.class),
-                new InputsDto("third", SubComplexObject.class)
-            );
-    }
 }
