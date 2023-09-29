f442f74f0615df34a68bc28b1878ac4a38a6464a
nbrouand <7816908+nbrouand@users.noreply.github.com>
2023-09-29 15:22:15 +0200

    chore(): Delete component folder

--- component/src/main/java/com/chutneytesting/component/scenario/domain/ComposableStepNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.chutneytesting.component.scenario.domain;
-
-public class ComposableStepNotFoundException extends RuntimeException {
-    public ComposableStepNotFoundException(String id) {
-        super("Composable step id [" + id + "] could not be found");
-    }
-}
