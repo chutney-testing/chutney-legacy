840571325b94794336832368fd222a8b0e38576b
boddissattva <58807088+boddissattva@users.noreply.github.com>
2023-10-13 13:20:45 +0200

    chore(server): Add javadoc to PurgeServiceImpl

--- server/src/main/java/com/chutneytesting/execution/domain/PurgeServiceImpl.java
+++ server/src/main/java/com/chutneytesting/execution/domain/PurgeServiceImpl.java
@@ -20,6 +20,7 @@ import com.chutneytesting.server.core.domain.scenario.campaign.Campaign;
 import com.chutneytesting.server.core.domain.scenario.campaign.CampaignExecutionReport;
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -80,8 +81,8 @@ public class PurgeServiceImpl implements PurgeService {
             }
 
             @Override
-            public Collection<Long> findExtraExecutionsIdsToDelete() {
-                var oldestCampaignExecutionToKept = currentTimeSortedExecutions().get(maxCampaignExecutionsConfiguration - 1);
+            public Collection<Long> findExtraExecutionsIdsToDelete(List<CampaignExecutionReport> timeSortedExecutionsForOneEnvironment) {
+                var oldestCampaignExecutionToKept = timeSortedExecutionsForOneEnvironment.get(maxCampaignExecutionsConfiguration - 1);
                 return ofNullable(campaignExecutionsByPartialExecution.get(true)).orElse(emptyList()).stream()
                     .filter(cer -> cer.startDate.isBefore(oldestCampaignExecutionToKept.startDate))
                     .map(cer -> cer.executionId)
@@ -104,19 +105,55 @@ public class PurgeServiceImpl implements PurgeService {
         return new PurgeReport(purgedScenariosExecutionsIds, purgedCampaignsExecutionsIds);
     }
 
+    /**
+     * Core logic to purge executions.
+     *
+     * @see #purgeExecutions()
+     */
     private static class PurgeExecutionService<ObjectType, ObjectIdType, ExecutionReportType> {
+        /**
+         * The configuration defining the number of executions to keep
+         */
         private final int maxExecutionsToKeep;
+        /**
+         * A supplier of a domain object related to executions.
+         *
+         * @see TestCaseMetadata
+         * @see Campaign
+         */
         private final Supplier<List<ObjectType>> baseObject;
+        /**
+         * A mapper function extracting the domain object id
+         */
         private final Function<ObjectType, ObjectIdType> idFunction;
+        /**
+         * A function returning all executions given an ObjectTypeId
+         */
         private final Function<ObjectIdType, List<ExecutionReportType>> executionsFunction;
+        /**
+         * An optional executions filter used in {@link #purgeExecutions()} before grouping by environment
+         */
         private final Predicate<ExecutionReportType> executionsFilter;
+        /**
+         * A mapper function extracting the execution id
+         */
         private final Function<ExecutionReportType, Long> executionIdFunction;
+        /**
+         * A mapper function extracting the execution date for sorting
+         */
         private final Function<ExecutionReportType, LocalDateTime> executionDateFunction;
+        /**
+         * A mapper function extracting the execution status for keeping the last success one
+         */
         private final Function<ExecutionReportType, ServerReportStatus> statusFunction;
+        /**
+         * A mapper function extracting the execution environment
+         */
         private final Function<ExecutionReportType, String> environmentFunction;
+        /**
+         * A function deleting executions by ids
+         */
         private final Function<Set<Long>, Set<ExecutionReportType>> deleteFunction;
-
-        private List<ExecutionReportType> timeSortedExecutionsForOneEnvironment;
 
         private PurgeExecutionService(
             int maxExecutionsToKeep,
@@ -142,6 +179,16 @@ public class PurgeServiceImpl implements PurgeService {
             this.deleteFunction = deleteFunction;
         }
 
+        /**
+         * Purge executions.
+         * <p> Find all base objects ids and map them to executions.</p>
+         * <p>For each group of executions, filter then group them by environment.</p>
+         * <p>For each group of executions by environment,</p>
+         * <p>permits a specific handle via {@link #handleExecutionsForOneEnvironment(List)}</p>
+         * <p>then call {@link #purgeOldestExecutionsFromOneEnvironment(List)}</p>
+         *
+         * @return The list of deleted executions ids.
+         */
         Set<Long> purgeExecutions() {
             Set<Long> deletedExecutionsIds = new HashSet<>();
             baseObject.get().stream()
@@ -162,16 +209,23 @@ public class PurgeServiceImpl implements PurgeService {
             return deletedExecutionsIds;
         }
 
-        protected final List<ExecutionReportType> currentTimeSortedExecutions() {
-            return timeSortedExecutionsForOneEnvironment;
-        }
-
+        /**
+         * Purge the oldest executions according to {@link #maxExecutionsToKeep} configuration
+         * by keeping the last success execution no matter what. <br/>
+         * Check for existence.
+         * Sort by date.
+         * Call {@link #findOldestExecutionsIdsWhileKeepingTheLastSuccess(List)}.
+         * Permits to add other executions ids via {@link #findExtraExecutionsIdsToDelete(List)}.
+         * Delete executions.
+         *
+         * @return The list of deleted executions ids.
+         */
         private Set<Long> purgeOldestExecutionsFromOneEnvironment(List<ExecutionReportType> executionsFromOneEnvironment) {
             if (executionsFromOneEnvironment.size() <= maxExecutionsToKeep) {
                 return emptySet();
             }
 
-            timeSortedExecutionsForOneEnvironment = executionsFromOneEnvironment.stream()
+            List<ExecutionReportType> timeSortedExecutionsForOneEnvironment = executionsFromOneEnvironment.stream()
                 .sorted(comparing(executionDateFunction).reversed())
                 .toList();
 
@@ -182,7 +236,7 @@ public class PurgeServiceImpl implements PurgeService {
             );
 
             deletedExecutionsIdsTmp.addAll(
-                findExtraExecutionsIdsToDelete()
+                findExtraExecutionsIdsToDelete(timeSortedExecutionsForOneEnvironment)
             );
 
             if (!deletedExecutionsIdsTmp.isEmpty()) {
@@ -218,11 +272,24 @@ public class PurgeServiceImpl implements PurgeService {
             return -1L;
         }
 
+        /**
+         * To implement in order to control the list of executions of one {@link #baseObject} for one environment
+         * to be passed down to {@link #purgeOldestExecutionsFromOneEnvironment(List)}. <br/>
+         * This list is not sorted.
+         *
+         * @return Default implementation returns the same list.
+         */
         protected List<ExecutionReportType> handleExecutionsForOneEnvironment(List<ExecutionReportType> executionsFromOneEnvironment) {
             return executionsFromOneEnvironment;
         }
 
-        protected Collection<Long> findExtraExecutionsIdsToDelete() {
+        /**
+         * To implement to find extra executions' ids to delete.
+         *
+         * @param timeSortedExecutionsForOneEnvironment The current list of executions processed by {@link #purgeOldestExecutionsFromOneEnvironment(List)}, sorted by {@link #executionDateFunction}
+         * @return Default implementation returns a {@link Collections#emptySet()}
+         */
+        protected Collection<Long> findExtraExecutionsIdsToDelete(List<ExecutionReportType> timeSortedExecutionsForOneEnvironment) {
             return emptySet();
         }
     }
