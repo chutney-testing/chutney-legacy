4603784ac92ffe652f26028eb057781da39007bd
DelaunayAlex <delaunayalexandre9518@gmail.com>
2024-01-04 15:56:29 +0100

    fix(): Resolve vars in name of retry strategy parent step

--- engine/src/test/java/com/chutneytesting/engine/domain/execution/strategies/RetryWithTimeOutStrategyTest.java
+++ engine/src/test/java/com/chutneytesting/engine/domain/execution/strategies/RetryWithTimeOutStrategyTest.java
@@ -16,6 +16,7 @@
 
 package com.chutneytesting.engine.domain.execution.strategies;
 
+import static com.chutneytesting.engine.api.execution.StatusDto.SUCCESS;
 import static com.chutneytesting.engine.domain.execution.ScenarioExecution.createScenarioExecution;
 import static java.util.List.of;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,9 +32,16 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.chutneytesting.ExecutionConfiguration;
+import com.chutneytesting.engine.api.execution.ExecutionRequestDto;
+import com.chutneytesting.engine.api.execution.StepExecutionReportDto;
+import com.chutneytesting.engine.api.execution.TestEngine;
 import com.chutneytesting.engine.domain.execution.ScenarioExecution;
+import com.chutneytesting.engine.domain.execution.engine.evaluation.StepDataEvaluator;
+import com.chutneytesting.engine.domain.execution.engine.scenario.ScenarioContextImpl;
 import com.chutneytesting.engine.domain.execution.engine.step.Step;
 import com.chutneytesting.engine.domain.execution.report.Status;
+import com.chutneytesting.tools.Jsons;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
@@ -177,6 +185,7 @@ public class RetryWithTimeOutStrategyTest {
 
         when(rootStep.subSteps()).thenReturn(mockSteps);
         when(rootStep.isParentStep()).thenReturn(true);
+        when(rootStep.dataEvaluator()).thenReturn(new StepDataEvaluator(null));
 
         RetryWithTimeOutStrategy strategy = new RetryWithTimeOutStrategy();
 
@@ -184,7 +193,7 @@ public class RetryWithTimeOutStrategyTest {
         StepStrategyDefinition strategyDefinition = new StepStrategyDefinition("", properties);
         when(rootStep.strategy()).thenReturn(Optional.of(strategyDefinition));
 
-        strategy.execute(createScenarioExecution(null), rootStep, null, strategies);
+        strategy.execute(createScenarioExecution(null), rootStep, new ScenarioContextImpl(), strategies);
 
         strategiesMock.forEach(strat -> verify(strat, times(1)).execute(any(), any(), any(), any(), any()));
     }
@@ -210,6 +219,7 @@ public class RetryWithTimeOutStrategyTest {
 
         when(rootStep.subSteps()).thenReturn(Lists.newArrayList(step1, step2, step3));
         when(rootStep.isParentStep()).thenReturn(true);
+        when(rootStep.dataEvaluator()).thenReturn(new StepDataEvaluator(null));
 
         StepExecutionStrategies strategies = mock(StepExecutionStrategies.class);
         when(strategies.buildStrategyFrom(step1)).thenReturn(strategy1);
@@ -220,12 +230,28 @@ public class RetryWithTimeOutStrategyTest {
         StepStrategyDefinition strategyDefinition = new StepStrategyDefinition("", properties);
         when(rootStep.strategy()).thenReturn(Optional.of(strategyDefinition));
 
-        strategyUnderTest.execute(createScenarioExecution(null), rootStep, null, strategies);
+        strategyUnderTest.execute(createScenarioExecution(null), rootStep, new ScenarioContextImpl(), strategies);
 
         verify(strategy1, times(10)).execute(any(), eq(step1), any(), any(), eq(strategies));
         verify(strategy2, times(7)).execute(any(), eq(step2), any(), any(), eq(strategies));
         verify(strategy3, times(7)).execute(any(), eq(step3), any(), any(), eq(strategies));
     }
+
+    @Test
+    public void should_resolve_name_from_context_with_strategy_retry() {
+        // G
+        final TestEngine testEngine = new ExecutionConfiguration().embeddedTestEngine();
+        ExecutionRequestDto requestDto = Jsons.loadJsonFromClasspath("scenarios_examples/retry_strategy_step_with_name_resolver_from_context_put.json", ExecutionRequestDto.class);
+
+        // W
+        StepExecutionReportDto result = testEngine.execute(requestDto);
+
+        // T
+        assertThat(result).hasFieldOrPropertyWithValue("status", SUCCESS);
+        assertThat(result.steps).hasSize(2);
+        assertThat(result.steps.get(1).name).isEqualTo("Step 2 Parent : value");
+    }
+
 
     @ParameterizedTest
     @MethodSource("retryStepParameters")
